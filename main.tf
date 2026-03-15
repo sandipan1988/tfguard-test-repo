911552e7b1b11993eda3c6f054a24a05d097f20f
Sandipan Saha <sandipansaha.negd@digitalindia.gov.in>
2026-03-15 20:26:26 +0530

    Add GCP and OCI security flaws for Premium tier testing

--- main.tf
+++ main.tf
@@ -39,3 +39,51 @@ resource "aws_security_group" "secure_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# --- GCP TESTING (Premium Only) ---
+
+resource "google_storage_bucket" "test_bucket" {
+  name     = "test-bucket-no-ubla"
+  location = "US"
+  # Vulnerability: Uniform bucket-level access is disabled
+  uniform_bucket_level_access = false 
+}
+
+resource "google_compute_instance" "test_instance" {
+  name         = "test-instance"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    # Vulnerability: Assigning a public IP address
+    access_config {
+      # Empty block assigns an ephemeral external IP
+    }
+  }
+}
+
+# --- OCI TESTING (Premium Only) ---
+
+resource "oci_objectstorage_bucket" "public_bucket" {
+    compartment_id = "ocid1.compartment.oc1..test"
+    name           = "public_bucket"
+    namespace      = "test_ns"
+    # Vulnerability: Public access enabled
+    access_type    = "ObjectRead" 
+}
+
+resource "oci_database_autonomous_database" "test_db" {
+    compartment_id           = "ocid1.compartment.oc1..test"
+    db_name                  = "testdb"
+    cpu_core_count           = 1
+    data_storage_size_in_tbs = 1
+    # Vulnerability: Publicly accessible database
+    is_public                = true
+}
